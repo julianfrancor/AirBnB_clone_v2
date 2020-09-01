1212a0eca8c0df7a8d91300932c4df00801226af
julianfrancor <julianfrancor@gmail.com>
2020-09-01 12:54:17 -0500

    task 1: script that starts a Flask web application and handles the routing for / and /hbnb

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -19,6 +19,7 @@ def index():
     returns the message we want to display in the user’s browser."""
     return ('Hello HBNB!')
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """used to generate URLs for that particular function, and
@@ -26,5 +27,5 @@ def hbnb():
     return ('HBNB')
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
